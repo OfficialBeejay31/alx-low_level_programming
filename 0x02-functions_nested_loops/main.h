ed4ef81e9cbc58653cdc30c15b4029d5115b745b
Odumosu Bolaji <odmusbolaji31@gmail.com>
2023-02-21 16:39:57 -0800

    commit'
    '

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/** DEsc: Header file containing declarations for all function used 
+ * in the 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+void print_alpphabet(void)
+#endif

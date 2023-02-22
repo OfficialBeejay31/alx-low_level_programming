febeb3cb9e444f1fe7f28d8dfe24cc6c4bc762c9
Odumosu Bolaji <odmusbolaji31@gmail.com>
2023-02-22 04:32:45 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,11 @@
 #include "main.h"
-
-int main(void){
+/**
+ * main - this is the entry pint and the functio of the whole program 
+ * _putchar - to print the alphabet _putchar
+ * Return: ALways 0
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');

f35fd95edd56aed0208caa5a1336d848357ce9c5
Odumosu Bolaji <odmusbolaji31@gmail.com>
2023-02-24 14:42:21 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints numbers from 0 to 9
+ *
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n')
+}
+
+

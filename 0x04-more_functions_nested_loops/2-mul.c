70f254bc9da09a2da9e5af11013dd514e3898f10
Odumosu Bolaji <odmusbolaji31@gmail.com>
2023-02-24 14:29:49 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @a: an input character
+ *
+ * @b: an input character
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+

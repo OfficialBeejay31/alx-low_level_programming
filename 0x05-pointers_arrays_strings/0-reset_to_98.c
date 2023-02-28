258c510aaef49ef68f76b8189ff865df91ba2c25
Odumosu Bolaji <odmusbolaji31@gmail.com>
2023-02-27 16:36:14 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that resets the value
+ * of a variable through a pointer to 0=98
+ *
+ * @n : an input integer
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
